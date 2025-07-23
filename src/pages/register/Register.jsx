d9b72f2299f5ea4b788c607e764fdc6520cee6b5
Amaia <amaiavzqz@gmail.com>
2025-07-23 11:17:04 +0200

    forms

--- src/pages/register/Register.jsx
+++ src/pages/register/Register.jsx
@@ -1,7 +1,30 @@
-import React from 'react'
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
 
 export const Register = () => {
   return (
-    <div>Register</div>
+    <section className='py-5 d-flex justify-content-center section-home'>
+      <Form className='w-25 bg-dark p-3 rounded-3 text-white'>
+        <h4 className='text-center mb-3'>Register form</h4>
+        <Form.Group className="mb-3" controlId="formGroupEmail">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control type="email" placeholder="Enter email" />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formGroupPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control type="password" placeholder="Password" />
+        </Form.Group>
+        <div className='text-center'>
+            <Button
+              className='mx-3'
+              variant="warning"
+            >Submit</Button>
+            <Button
+              className='mx-3'
+              variant="warning"
+            >Cancel</Button>
+          </div>
+      </Form>
+    </section>
   )
 }
