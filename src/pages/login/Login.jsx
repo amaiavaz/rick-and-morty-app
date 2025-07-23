d32d8b5c8b47746b4cdaf4ab153afb3f0c94decc
Amaia <amaiavzqz@gmail.com>
2025-07-23 11:32:01 +0200

    register control

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -8,11 +8,11 @@ export const Login = () => {
         <h4 className='text-center mb-3'>Login form</h4>
         <Form.Group className="mb-3" controlId="formGroupEmail">
           <Form.Label>Email address</Form.Label>
-          <Form.Control type="email" placeholder="Enter email" />
+          <Form.Control type="text" placeholder="Enter email" />
         </Form.Group>
         <Form.Group className="mb-3" controlId="formGroupPassword">
           <Form.Label>Password</Form.Label>
-          <Form.Control type="password" placeholder="Password" />
+          <Form.Control type="text" placeholder="Password" />
         </Form.Group>
         <div className='text-center'>
             <Button
--- src/pages/register/Register.jsx
+++ src/pages/register/Register.jsx
@@ -1,18 +1,54 @@
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
+import { useState } from 'react';
+
+const initialValue = {
+  name:"",
+  email:"",
+  password:""
+}
 
 export const Register = () => {
+  const [register, setRegister] = useState(initialValue);
+
+  const handleChange = (e) => {
+    const {name, value} = e.target;
+    setRegister({...register, [name]: value});
+  }
+
   return (
     <section className='py-5 d-flex justify-content-center section-home'>
       <Form className='w-25 bg-dark p-3 rounded-3 text-white'>
         <h4 className='text-center mb-3'>Register form</h4>
         <Form.Group className="mb-3" controlId="formGroupEmail">
+          <Form.Label>Name</Form.Label>
+          <Form.Control 
+            type="text" 
+            placeholder="Enter name"
+            name='name'
+            value={register.name}
+            onChange={handleChange}
+          />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formGroupEmail">
           <Form.Label>Email address</Form.Label>
-          <Form.Control type="email" placeholder="Enter email" />
+          <Form.Control 
+            type="text"
+            placeholder="Enter email" 
+            name='email'
+            value={register.email}
+            onChange={handleChange}
+          />
         </Form.Group>
         <Form.Group className="mb-3" controlId="formGroupPassword">
           <Form.Label>Password</Form.Label>
-          <Form.Control type="password" placeholder="Password" />
+          <Form.Control 
+            type="text" 
+            placeholder="Password"
+            name='password'
+            value={register.password}
+            onChange={handleChange}
+          />
         </Form.Group>
         <div className='text-center'>
             <Button
